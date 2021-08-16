f0d5d1cce8bb2adc8efb1da708c9cfb8531bf952
Amjed Tahir <a.tahir@massey.ac.nz>
2021-08-17 09:01:14 +1200

    added jaxb examples

---
+++ jaxb/generatejava.sh
@@ -0,0 +1,3 @@
+# script to generated the API classes
+# to see this in action, delete package nz.ac.massey.cs.sdc.jaxb first
+xjc -d src -p nz.ac.massey.cs.sdc.jaxb email.xsd
---
+++ jaxb/src/test/nz/ac/massey/cs/sdc/jaxb/TestParser.java
@@ -0,0 +1,41 @@
+package test.nz.ac.massey.cs.sdc.jaxb;
+
+import static org.junit.Assert.*;
+import java.io.File;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.Unmarshaller;
+import nz.ac.massey.cs.sdc.jaxb.*;
+import org.junit.Test;
+
+/**
+ * Test whether the jaxb-generated parser works correctly.
+ * Run this with junit.
+ * @author jens dietrich
+ */
+public class TestParser {
+
+	@Test
+	public void test() throws Exception {
+		JAXBContext jc = JAXBContext.newInstance("nz.ac.massey.cs.sdc.jaxb");
+		Unmarshaller parser = jc.createUnmarshaller();
+		File file = new File("email1.xml");
+		Email mail = (Email) parser.unmarshal(file);
+
+		assertEquals(1,mail.getTo().size());
+		Participant to = mail.getTo().get(0);
+		assertEquals("staff@massey.ac.nz",to.getEmailAddress());
+		assertEquals("John John",to.getDisplayName());
+
+		assertEquals(1,mail.getCc().size());
+		Participant cc = mail.getCc().get(0);
+		assertEquals("students159251@massey.ac.nz",cc.getEmailAddress());
+		assertEquals("Sarah Jonh",cc.getDisplayName());
+
+		assertEquals(0,mail.getBcc().size());
+
+		assertEquals("update",mail.getSubject());
+		assertEquals("this lecture notes have been updated",mail.getBody());
+
+	}
+
+}
